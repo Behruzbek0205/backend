d4281144c6e457b97fa557b78147ff657bdcc746
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2026-01-02 11:55:51 +0500

    house route

--- routes/house.route.js
+++ routes/house.route.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const houseRoute = express.Router();
-const { createHouse, houseGet, houseGetByID ,updateHouse, deleteHouse, houseSearch} = require("../controller/houseController");
+const {
+  createHouse,
+  houseGet,
+  houseGetByID,
+  updateHouse,
+  deleteHouse,
+  houseSearch,
+} = require("../controller/houseController");
 
-
-
+const {
+  houseValidationshceme,
+  houseupdateValidationshceme,
+} = require("../validation/houseValidation");
 
 houseRoute.post("/houseCreate", createHouse);
 houseRoute.get("/houseGet", houseGet);
-houseRoute.get("/getHouseById/:id" , houseGetByID)
-houseRoute.put("/updateHouse/:id" , updateHouse)
-houseRoute.delete("/deleteHouse/:id" , deleteHouse)
-houseRoute.get("/houseSearch" , houseSearch)
+houseRoute.get("/getHouseById/:id", houseGetByID);
+houseRoute.put("/updateHouse/:id", updateHouse);
+houseRoute.delete("/deleteHouse/:id", deleteHouse);
+houseRoute.get("/houseSearch", houseSearch);
 
 module.exports = { houseRoute };
