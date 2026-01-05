5841d9554b66c44d354d7c04ef0a99a09ba10894
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2026-01-05 15:34:45 +0500

    swwagger

--- routes/user.route.js
+++ routes/user.route.js
@@ -15,16 +15,64 @@ const {
   updateUserValidationScheme,
 } = require("../validation/userValidation");
 
-
 const validationScheme = (sheme) => (req, res, next) => {
-  const {error} = sheme.validate(req.body)
-  if(error) {
-    return res.status(400).send(error.details[0].message)
+  const { error } = sheme.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
-  next()
-}
+  next();
+};
+/**
+ * @swagger
+ * name: Users
+ * description:
+ */
 
-
+/**
+ * @swagger
+ * /users/CreateUser:
+ * post:
+ * summary: Yangi foydalanuvchini yaratish
+ * tags: [Users]
+ * description: Yangi faydalanuvchini yaratish
+ * requestBody:
+ * reqiured: true
+ * content:
+ * application/json"
+ * shema:
+ * type:object
+ * properties: 
+ * username:
+ * type:string
+ * description: Foydalanuvchining yagona username
+ * password: 
+ * type: string
+ * description: Foydalanuvchining akkounti uchun parol
+ * firstname:
+ * type:string
+ * description: Foydalanuvchining ismi
+ * lastname:
+ * type:string
+ * description: Foydalanuvchini familiyasi
+ * birthday: 
+ * type: String
+ * description: Foydalanuvchining yili
+ * phone: 
+ * type:string
+ * description: Foydalanuvchining telefon nomerin
+ * addres: 
+ * type:string
+ * description: Foydalanuvchining addressi
+ * gender: 
+ * type:string 
+ * responses:
+ * '201':
+ * description: Foydalanuvchini muvaffaqiyatli royxatdan otdi
+ * '400':
+ * description: Yomon sorov, validatsiya xatosi
+ * '500':
+ * description:Server xatosi
+ */
 
 userRoute.post(
   "/CreateUser",
